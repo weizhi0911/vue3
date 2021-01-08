a6345c97e5ec06084d8d72a27b1dcb418aa7a2af
git config --global user.name <git config --global user.email>
2021-01-08 17:28:59 +0800

    '跟新'

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <HelloWorld2 msg="Welcome to Your Vue.js + TypeScript App" /> -->
+  <modal />
+  <div v-if="loading">.....Loading</div>
+  <img v-if="loaded" :src="result.message" />
+
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
+import UserUrlAxioa from './Hooks/UserUrlAxioa'
 import HelloWorld from './components/HelloWorld.vue';
-
+// import HelloWorld2 from './components/HelloWorld2.vue'
+import modal from "./components/Modal.vue";
 export default defineComponent({
   name: 'App',
+  setup() {
+    const { result, loaded, loading } = UserUrlAxioa('https://dog.ceo/api/breeds/image/random')
+    return {
+      result,
+      loaded,
+      loading,
+    }
+  },
   components: {
-    HelloWorld
-  }
-});
+    HelloWorld,
+    // HelloWorld2,
+    modal
+  },
+})
 </script>
 
 <style>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">{{nowTime}}</div>
+  <button @click="getNowTime">触发时间</button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup() {
+    const nowTime = ref('00:00:00')
+    const getNowTime = () => {
+      const now = new Date()
+      const hour = now.getHours() < 10 ? 0 + now.getHours() : now.getHours()
+      const minu =
+        now.getMinutes() < 10 ? 0 + now.getMinutes() : now.getMinutes()
+      const sec =
+        now.getSeconds() < 10 ? 0 + now.getSeconds() : now.getSeconds()
+      nowTime.value = hour + ':' + minu + ':' + sec
+      setTimeout(getNowTime, 1000)
+    }
+    return {
+      nowTime,
+      getNowTime,
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+ 
